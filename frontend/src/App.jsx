a688e39baf2aedcd29db389b0811609015ef5831
Sawan <itissaavn@gmail.com>
2024-03-31 12:45:25 +0530

    Add: Signup Signin routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<h1>Signin</h1>} />
+        <Route path="signup" element={<h1>Signup</h1>} />
+      </Route>
+      {/* <Route path="/editor" element={<Editor />} /> */}
+    </Routes>
+  );
 };
 
 export default App;
