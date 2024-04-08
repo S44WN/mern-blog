b30b54249adaed05a817a8e49bb722d8e8bc7b9d
Saawn <itissaavn@gmail.com>
2024-04-08 15:33:26 +0530

    Add: google-auth

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -7,6 +7,7 @@ import { Toaster, toast } from "react-hot-toast";
 import axios from "axios";
 import { storeInSession } from "../common/session";
 import { UserContext } from "../App";
+import { signInWithGoogle } from "../common/firebase";
 
 const UserAuthForm = ({ type }) => {
   let {
@@ -71,6 +72,24 @@ const UserAuthForm = ({ type }) => {
     userAuthThroughServer(serverRoute, formData);
   };
 
+  const handleGoogleAuth = async (e) => {
+    e.preventDefault();
+
+    await signInWithGoogle()
+      .then((user) => {
+        let serverRoute = "/google-auth";
+
+        let formData = {
+          access_token: user.accessToken,
+        };
+
+        userAuthThroughServer(serverRoute, formData);
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
   return access_token ? (
     <Navigate to="/" />
   ) : (
@@ -124,7 +143,10 @@ const UserAuthForm = ({ type }) => {
             <hr className="w-1/2 border-black"></hr>
           </div>
 
-          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+          <button
+            className="btn-dark flex items-center justify-center gap-4 w-[90%] center"
+            onClick={handleGoogleAuth}
+          >
             <img src={googleIcon} alt="Google Icon" className="w-5" />
             Continue with Google
           </button>
