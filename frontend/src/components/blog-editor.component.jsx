dd4658e5314f7eef994ba323bc821c6cd4b39327
Saawn <itissaavn@gmail.com>
2024-04-09 16:13:11 +0530

    Add: image upload(frontend, extraction)

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
+import AnimationWrapper from "../common/page-animation";
+import defaultBanner from "../imgs/blog banner.png";
 
 const BlogEditor = () => {
+  const handleBannerUpload = (e) => {
+    let img = e.target.files[0];
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -16,6 +22,25 @@ const BlogEditor = () => {
           <button className="btn-light py-2">Save Draft</button>
         </div>
       </nav>
+
+      <AnimationWrapper>
+        <section>
+          <div className="mx-auto max-w-[900px] w-full">
+            <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+              <label htmlFor="uploadBanner">
+                <img src={defaultBanner} alt="blog banner" className="z-20" />
+                <input
+                  id="uploadBanner"
+                  type="file"
+                  accept=".png, .jpg, .jpeg"
+                  hidden
+                  onClick={handleBannerUpload}
+                />
+              </label>
+            </div>
+          </div>
+        </section>
+      </AnimationWrapper>
     </>
   );
 };
